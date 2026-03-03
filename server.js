8a7f9d1dade1d8ae60fdc6a257cafde166dfe668
anthonyarimalala <119404637+anthonyarimalala@users.noreply.github.com>
2026-03-03 09:21:08 +0300

    Change server port from 5000 to 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const uploadRoute = require("./routes/upload");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
